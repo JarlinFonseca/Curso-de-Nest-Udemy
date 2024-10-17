0dacc14d6a7132f6e089262d4b0421cbadb1a9f9
Jarlin Fonseca <j.fonseca@nelumbo.com.co>
2024-10-17 17:57:13 -0500

    Obtener carro por ID

--- 02-car-dealership/src/cars/cars.controller.ts
+++ 02-car-dealership/src/cars/cars.controller.ts
@@ -1,9 +1,33 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  BadRequestException,
+  Controller,
+  Get,
+  NotFoundException,
+  Param,
+} from '@nestjs/common';
 
 @Controller('cars')
 export class CarsController {
+  private cars = ['Toyota', 'Honda', 'Jeep'];
   @Get()
   getAllCars() {
-    return ['Toyota', 'Honda', 'Jeep'];
+    return this.cars;
+  }
+
+  @Get(':id')
+  getCarById(@Param('id') id: number) {
+    if (id > this.cars.length) {
+      throw new NotFoundException({
+        message: 'Car not found',
+        code: 'ERR_CAR_NOT_FOUND',
+      });
+    } else if (id <= 0) {
+      throw new BadRequestException({
+        message: 'Invalid car ID',
+        code: 'ERR_INVALID_ID',
+      });
+    }
+
+    return this.cars[--id];
   }
 }
