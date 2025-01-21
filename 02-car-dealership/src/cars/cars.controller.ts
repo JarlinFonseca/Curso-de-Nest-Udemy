892c44905327eff0643aea0e051a500ff7974e39
Jarlin Fonseca <j.fonseca@nelumbo.com.co>
2025-01-20 20:30:55 -0500

    Servicios e inyeccion de dependencias

--- 02-car-dealership/src/cars/cars.controller.ts
+++ 02-car-dealership/src/cars/cars.controller.ts
@@ -1,33 +1,17 @@
-import {
-  BadRequestException,
-  Controller,
-  Get,
-  NotFoundException,
-  Param,
-} from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { CarsService } from './cars.service';
 
 @Controller('cars')
 export class CarsController {
-  private cars = ['Toyota', 'Honda', 'Jeep'];
+  constructor(private readonly carsService: CarsService) {}
+
   @Get()
   getAllCars() {
-    return this.cars;
+    return this.carsService.findAll();
   }
 
   @Get(':id')
-  getCarById(@Param('id') id: number) {
-    if (id > this.cars.length) {
-      throw new NotFoundException({
-        message: 'Car not found',
-        code: 'ERR_CAR_NOT_FOUND',
-      });
-    } else if (id <= 0) {
-      throw new BadRequestException({
-        message: 'Invalid car ID',
-        code: 'ERR_INVALID_ID',
-      });
-    }
-
-    return this.cars[--id];
+  getCarById(@Param('id') id: string) {
+    return this.carsService.findOneById(+id);
   }
 }
